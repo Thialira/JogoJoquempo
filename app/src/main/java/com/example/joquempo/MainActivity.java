51a4dd310263716be4315b39d3a70c899988c0f7
Thiago Mendes <tjlira07@gmail.com>
2024-04-04 11:14:24 -0300

    Primeira parte - Atualização do jogo

--- app/src/main/java/com/example/joquempo/MainActivity.java
+++ app/src/main/java/com/example/joquempo/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.joquempo;
 
+import static com.example.joquempo.R.*;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -44,6 +46,11 @@ public class MainActivity extends AppCompatActivity {
         ImageView imageResultado = findViewById(R.id.imageResultado);
         TextView textResultado = findViewById(R.id.textResultado);
 
+        // Resetar a opacidade de todas as imagens
+        findViewById(R.id.imagePedra).setAlpha(1.0f);
+        findViewById(R.id.imagePapel).setAlpha(1.0f);
+        findViewById(R.id.imageTesoura).setAlpha(1.0f);
+
         int numero = new Random().nextInt(3);
         String[] opcoes = {"pedra", "papel", "tesoura"};
         String opcaoApp = opcoes[numero];
@@ -62,20 +69,72 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
 
-        if ((opcaoApp == "tesoura" && opcaoSelecionada == "papel") ||
-                (opcaoApp == "papel" && opcaoSelecionada == "pedra") ||
-                (opcaoApp == "pedra" && opcaoSelecionada == "tesoura")
-        ) {
-            textResultado.setText("Voce Perdeu !!");
-        } else if ((opcaoSelecionada == "tesoura" && opcaoApp == "papel") ||
-                (opcaoSelecionada == "papel" && opcaoApp == "pedra") ||
-                (opcaoSelecionada == "pedra" && opcaoApp == "tesoura")
-        ) {
-            textResultado.setText("Voce Ganhou!!");
+        // Destacar a opção selecionada pelo usuário
+        switch (opcaoSelecionada) {
+            case "pedra":
+                findViewById(R.id.imagePedra).setAlpha(0.5f); // Reduz a opacidade para 50%
+                break;
+
+            case "papel":
+                findViewById(R.id.imagePapel).setAlpha(0.5f); // Reduz a opacidade para 50%
+                break;
+
+            case "tesoura":
+                findViewById(R.id.imageTesoura).setAlpha(0.5f); // Reduz a opacidade para 50%
+                break;
+        }
+
+        if ((opcaoApp.equals("tesoura") && opcaoSelecionada.equals("papel")) ||
+                (opcaoApp.equals("papel") && opcaoSelecionada.equals("pedra")) ||
+                (opcaoApp.equals("pedra") && opcaoSelecionada.equals("tesoura"))) {
+            textResultado.setText("Você Perdeu !!");
+        } else if ((opcaoSelecionada.equals("tesoura") && opcaoApp.equals("papel")) ||
+                (opcaoSelecionada.equals("papel") && opcaoApp.equals("pedra")) ||
+                (opcaoSelecionada.equals("pedra") && opcaoApp.equals("tesoura"))) {
+            textResultado.setText("Você Ganhou!!");
         } else {
             textResultado.setText("Empatamos !!");
         }
-
     }
+
+
+//    public void opcaoSelecionada(String opcaoSelecionada) {
+//
+//        ImageView imageResultado = findViewById(R.id.imageResultado);
+//        TextView textResultado = findViewById(R.id.textResultado);
+//
+//        int numero = new Random().nextInt(3);
+//        String[] opcoes = {"pedra", "papel", "tesoura"};
+//        String opcaoApp = opcoes[numero];
+//
+//        switch (opcaoApp) {
+//            case "pedra":
+//                imageResultado.setImageResource(R.drawable.pedra);
+//                break;
+//
+//            case "papel":
+//                imageResultado.setImageResource(R.drawable.papel);
+//                break;
+//
+//            case "tesoura":
+//                imageResultado.setImageResource(R.drawable.tesoura);
+//                break;
+//        }
+//
+//        if ((opcaoApp == "tesoura" && opcaoSelecionada == "papel") ||
+//                (opcaoApp == "papel" && opcaoSelecionada == "pedra") ||
+//                (opcaoApp == "pedra" && opcaoSelecionada == "tesoura")
+//        ) {
+//            textResultado.setText("Voce Perdeu !!");
+//        } else if ((opcaoSelecionada == "tesoura" && opcaoApp == "papel") ||
+//                (opcaoSelecionada == "papel" && opcaoApp == "pedra") ||
+//                (opcaoSelecionada == "pedra" && opcaoApp == "tesoura")
+//        ) {
+//            textResultado.setText("Voce Ganhou!!");
+//        } else {
+//            textResultado.setText("Empatamos !!");
+//        }
+//
+//    }
 
 }
